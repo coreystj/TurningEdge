854bc0b3860621a8579af7d8691d6a172d058c8c
corey st-jacques <corey_stjacques@hotmail.com>
2018-06-02 22:49:38 -0400

    Created MakerWow engine. Cleaned up code and deleted modules.

---
+++ TurningEdge.MakerWow/TurningEdge.MakerWow/Models/MakerWowEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurningEdge.MakerWow.Models
+{
+    public class MakerWowEngine
+    {
+        public MakerWowEngine()
+        {
+            TurningEdge.Web.Unity.Factories.Static.MonoWebContextFactory.Create();
+        }
+    }
+}
